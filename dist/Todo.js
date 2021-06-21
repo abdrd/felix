4b27a5e648e44ba562b438e41b0daa5351a3041d
Abidin Durdu <abidindrd@gmail.com>
2021-06-21 15:10:51 +0300

    .gitignore

--- dist/Todo.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var Todo = /** @class */ (function () {
-    function Todo(text) {
-        this.text = text;
-    }
-    return Todo;
-}());
-exports.default = Todo;
